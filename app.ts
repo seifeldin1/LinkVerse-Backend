b74642799620825075cace044d48c9f98a702eaf
seifeldin1 <seif1442004@gmail.com>
2025-10-18 23:50:30 +0300

    initialized backend

---
+++ app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello 🌍 from your TypeScript Express server!");
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
